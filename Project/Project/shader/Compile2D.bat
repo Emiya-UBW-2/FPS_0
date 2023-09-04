c2f2e7d07ed8f248d4358c9f9ff258f28ec90dc2
emiya_ubw <a6m3.zero.f@gmail.com>
2023-09-04 22:47:52 +0900

    フォルダ移動

--- Project/Project/shader/Compile2D.bat
+++
@@ -1,5 +0,0 @@
-ShaderCompiler.exe /Tvs_4_0 DepthVS.fx
-ShaderCompiler.exe /Tps_4_0 DepthPS.fx
-ShaderCompiler.exe /Tvs_4_0 NormalMesh_DirLight_PhongVS.fx
-ShaderCompiler.exe /Tps_4_0 NormalMesh_DirLight_PhongPS.fx
-pause
